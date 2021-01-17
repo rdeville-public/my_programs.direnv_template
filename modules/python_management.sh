40c0e58bdd668f2d47c4ecd277e8937eab99932b
Romain Deville <contact@romaindeville.fr>
2021-01-17 22:59:28 +0100

    💡🐛 Improve documentation and pining dep for python module
    Update source code documentation to be simplier for python_management module.
    Update pining dependencies method to remove ${DIRENV_ROOT} from path to
    requirements.*.in in requirements.*.txt

--- modules/python_management.sh
+++ modules/python_management.sh
@@ -51,28 +51,18 @@
 #   Possible values are:
 #
 #   - `dev` (default), will process following files:
-#       - `requirements.dev.in`: Contain unpinned python dependencies used for
-#         development purpose.
-#       - `requirements.dev.txt`: Contain pinned python dependencies used for
-#         development purpose.
-#       - `requirements.docs.in`: Contain unpinned python dependencies used for
-#         documentation purpose.
-#       - `requirements.docs.txt`: Contain pinned python dependencies used for
-#         documentation purpose.
-#       - `requirements.prod.in`: Contain unpinned python dependencies used for
-#         production purpose.
-#       - `requirements.prod.txt`: Contain pinned python dependencies used for
-#         production purpose.
+#       - `requirements.dev.*`: Contain unpinned (.in) and pinned (.txt) python
+#         dependencies used for development purpose.
+#       - `requirements.docs.*`: Contain unpinned (.in) and pinned (.txt) python
+#         dependencies used for documentation purpose.
+#       - `requirements.prod.*`: Contain unpinned (.in) and pinned (.txt) python
+#         dependencies used for production purpose.
 #   - `docs` (default), will process following files:
-#       - `requirements.docs.in`: Contain unpinned python dependencies used for
-#         documentation purpose.
-#       - `requirements.docs.txt`: Contain pinned python dependencies used for
-#         documentation purpose.
+#       - `requirements.docs.*`: Contain unpinned (.in) and pinned (.txt) python
+#         dependencies used for documentation purpose.
 #   - `prod`, will process following files:
-#       - `requirements.prod.in`: Contain unpinned python dependencies used for
-#         production purpose.
-#       - `requirements.prod.txt`: Contain pinned python dependencies used for
-#         production purpose.
+#       - `requirements.prod.*`: Contain unpinned (.in) and pinned (.txt) python
+#         dependencies used for production purpose.
 #
 #   ## `.envrc.ini` example
 #
@@ -294,6 +284,8 @@ python_management()
     then
       direnv_log "INFO" "Generation of the python ${type_requirements} requirements with pinned version."
       pip-compile "${unpin_requirements}" >> "${DIRENV_LOG_FOLDER}/module.python_management.log" 2>&1
+      # Remove ${DIRENV_ROOT} from pinned version of the requirements
+      sed -i -e "s/${DIRENV_ROOT//\//\\\/}\///g" ${pinned_requirements}
     fi
   }
 
